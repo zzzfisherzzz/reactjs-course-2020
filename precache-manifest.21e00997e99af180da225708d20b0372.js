099cd09408603aeed2ff50c86c7a2be08ed5e3f0
zzzfisherzzz <zzzfisherzzz@gmail.com>
2020-09-24 09:20:58 +0300

    Updates

---
+++ precache-manifest.21e00997e99af180da225708d20b0372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f500e8a15b9ecf9c4f8156f0a27aea7f",
+    "url": "/reactjs-course-2020/index.html"
+  },
+  {
+    "revision": "0d26d7bcae39a4e5b600",
+    "url": "/reactjs-course-2020/static/css/main.9e71ff5d.chunk.css"
+  },
+  {
+    "revision": "185d21e950a3c40ce0b4",
+    "url": "/reactjs-course-2020/static/js/2.8ef66167.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-course-2020/static/js/2.8ef66167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0389d6d21f50645b272",
+    "url": "/reactjs-course-2020/static/js/3.8d17c2fb.chunk.js"
+  },
+  {
+    "revision": "0d26d7bcae39a4e5b600",
+    "url": "/reactjs-course-2020/static/js/main.c81adf44.chunk.js"
+  },
+  {
+    "revision": "1187e6286fedef0f5ae1",
+    "url": "/reactjs-course-2020/static/js/runtime-main.d390e1f3.js"
+  }
+]);
